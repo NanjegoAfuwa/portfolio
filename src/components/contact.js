3e46c415188cf1dc99eceade29f7329e10d59524
NanjegoAfuwa <portablebirungi@gmail.com>
2025-09-24 13:44:47 +0300

    portfolio

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ export function Contact() {
     <section id="contact" className="contact">
       <h2>Contact Me</h2>
       <p>Email: <a href="mailto:nanjegoafuwa@gmail.com">nanjegoafuwa@gmail.com</a></p>
-      <p> <FaWhatsapp /> <a href="https://wa.me/256703273018" target="_blank" rel="noopener noreferrer">+256 703273028</a></p>
+      <p> <FaWhatsapp /> <a href="https://wa.me/256703273018" target="_blank" rel="noopener noreferrer">+256 703273018</a></p>
        <p> <FaPhoneAlt />  <a href="tel:+256765057288" target="_blank" rel="noopener noreferrer">+256 765057288</a></p>
 
       <div className="social-icons">
